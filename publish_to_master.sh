a2ee75b8f223b03ade1abe040007165828902ce0
Aidan Meacham <1ceaham@gmail.com>
2019-02-15 15:54:20 +0100

    Test copying circleci config

--- publish_to_master.sh
+++ publish_to_master.sh
@@ -1,4 +1,5 @@
 hugo -v # command to build your site
+cp -R .circleci public/.circleci # copy config to ignore master on next build
 cd public # cd into folder containing your built site
 
 git init
@@ -10,4 +11,4 @@ git add .
 git commit -m "Deploy from CircleCI" # this will always be the only commit in your master branch
 
 # use --quiet to avoid printing token in build logs
-git push --force --quiet "https://${CI_TOKEN}@github.com/seangransee/blog.git" source:master # path to your repo on GitHub, using token for authentication
+git push --force --quiet "https://${CI_TOKEN}@github.com/1ceaham/1ceaham.github.io.git" source:master # path to your repo on GitHub, using token for authentication
